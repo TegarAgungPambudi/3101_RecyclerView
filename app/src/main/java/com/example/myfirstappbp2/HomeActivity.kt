512bcb5a26855ddef4d4ed24752c52d4ea196977
Tegar <tegaragungpambudi@students.amikom.ac.id>
2025-11-03 00:14:39 +0700

    initial commit

---
+++ app/src/main/java/com/example/myfirstappbp2/HomeActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myfirstappbp2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+// HAPUS import LinearLayoutManager
+// import androidx.recyclerview.widget.LinearLayoutManager
+
+// TAMBAHKAN import GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val dataList = arrayListOf(
+            Mahasiswa("Fahmi", "23.12.2973"),
+            Mahasiswa("Yujin", "23.12.2978"),
+            Mahasiswa("Chaewon", "23.12.2983"),
+            Mahasiswa("Kazuha", "23.12.2889"),
+            Mahasiswa("seolhyun", "23.12.2877")
+        )
+
+        val rv_mahasiswa: RecyclerView = findViewById(R.id.rv_mahasiswa)
+
+        rv_mahasiswa.layoutManager = GridLayoutManager(this, 2)
+
+        val adapter = MahasiswaAdapter(dataList)
+
+        rv_mahasiswa.adapter =adapter}
+}
---
+++ app/src/main/java/com/example/myfirstappbp2/MahasiswaAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.myfirstappbp2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast // <-- TAMBAHKAN IMPORT INI
+import androidx.recyclerview.widget.RecyclerView
+
+class MahasiswaAdapter(private val listMahasiswa: List<Mahasiswa>) :
+    RecyclerView.Adapter<MahasiswaAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tv_nama: TextView = itemView.findViewById(R.id.tv_nama_mahasiswa)
+        val tv_nim: TextView = itemView.findViewById(R.id.tv_nim_mahasiswa)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_row_mahasiswa, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val mahasiswa = listMahasiswa[position]
+        holder.tv_nama.text = mahasiswa.nama
+        holder.tv_nim.text = mahasiswa.nim
+
+        holder.itemView.setOnClickListener {
+            Toast.makeText(
+                holder.itemView.context,
+                "Kamu memilih: ${mahasiswa.nama}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listMahasiswa.size
+        }
+}
